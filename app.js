9aca1456617a6909d0b8c736bc606d29d182f101
Yugant Kachroo <yugant35@gmail.com>
2020-06-15 11:56:34 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+const rootDir = require('../util/path');
+
+router.get('/add-product', (req, res) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+router.post('/add-product', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
