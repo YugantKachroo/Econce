34dfa7d5a6f1e9726387844f1d2ac95736a05496
Yugant Kachroo <yugant35@gmail.com>
2020-06-17 21:22:41 +0530

    delete route working

--- routes/admin.js
+++ routes/admin.js
@@ -23,6 +23,6 @@ router.get('/edit-product/:productId', getEditProduct);
 
 router.post('/edit-product', postEditProduct);
 
-// router.post('/delete-product', postDeleteProduct);
+router.post('/delete-product', postDeleteProduct);
 
 module.exports = router;
