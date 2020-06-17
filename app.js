f0aacf87872d11ebad68b65ecb99b663d25a759c
Yugant Kachroo <yugant35@gmail.com>
2020-06-17 20:05:30 +0530

    product model added

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const errorController = require('./controllers/error');
 
+const errorController = require('./controllers/error');
+// const User = require('./models/user');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -14,25 +15,24 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
