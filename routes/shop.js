0670faa405eaad301e70daefcfdd65756caba769
Yugant Kachroo <yugant35@gmail.com>
2020-06-18 16:23:24 +0530

    get card functionality added

--- routes/shop.js
+++ routes/shop.js
@@ -21,7 +21,7 @@ router.get('/products', getProducts);
 
 router.get('/products/:productId', getProduct);
 
-// router.get('/cart', getCart);
+router.get('/cart', getCart);
 
 router.post('/cart', postCart);
 
