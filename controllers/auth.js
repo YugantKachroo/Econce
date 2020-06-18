e80705c530e18726274a31fac576085938ea4b49
Yugant Kachroo <yugant35@gmail.com>
2020-06-18 22:33:08 +0530

    cookie delete functionality working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const User = require('../models/user');
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
@@ -7,6 +8,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('5eeb3d59b0f6d3415c1b9848')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getLogin, postLogin } = require('../controllers/auth');
+const { getLogin, postLogin, postLogout } = require('../controllers/auth');
 
 router.get('/login', getLogin);
 router.post('/login', postLogin);
+router.post('/logout', postLogout);
 
 module.exports = router;
