ffa57344912625806ab91ae05b66bec1ee4d5f4f
Yugant Kachroo <yugant35@gmail.com>
2020-06-19 19:56:13 +0530

    bug resolved

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,19 +40,21 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
-      return bcrypt.hash(password, 12);
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect('/login');
+        });
     })
-    .then((hashedPassword) => {
-      const user = new User({
-        email,
-        password: hashedPassword,
-        cart: { items: [] },
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.redirect('/login');
-    })
+
     .catch((err) => {
       console.log(err);
     });
