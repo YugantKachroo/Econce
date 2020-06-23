311c5a85f111e00ec6cc0ba2d681b30049712769
Yugant Kachroo <yugant35@gmail.com>
2020-06-23 11:52:11 +0530

    pagination working

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,6 +5,8 @@ const path = require('path');
 const pdfkit = require('pdfkit');
 const pdfInvoice = require('pdf-invoice');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then((products) => {
@@ -40,7 +42,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render('shop/index', {
         prods: products,
